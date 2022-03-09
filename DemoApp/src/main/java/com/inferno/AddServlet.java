8979a496013007b224a7ce04bd1bb3671de1a3fd
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-03-10 02:05:51 +0530

    servlet basics added

---
+++ DemoApp/src/main/java/com/inferno/AddServlet.java
@@ -0,0 +1,26 @@
+package com.inferno;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@SuppressWarnings("serial")
+public class AddServlet extends HttpServlet {
+	
+	// this method name must be service() for servlet to function properly
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+		
+		// System.out.println("The result is = " + k);
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("The result is = " + k);
+	}
+}
