59bf2506ba487783a6dc0d5589e95154b01af75e
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-03-19 01:14:02 +0530

    JDBC update

--- DemoJDBC/src/main/java/DemoClass.java
+++ DemoJDBC/src/main/java/DemoClass.java
@@ -20,31 +20,41 @@
  *  
  */
 
-import java.sql.*;															// Step-1
-import java.io.*;
+import java.sql.*;																// Step-1
 
 public class DemoClass {
 
 	public static void main(String[] args) throws Exception {
+				
+		// System.out.println("Hello World !!!");
 		
 		// Constants required to create a connection
-		String url = " ";
+		String url = "jdbc:mysql://localhost:3306/testdb2";
 		String uname = "root";
-		String pswrd = " ";
+		String pswrd = "admin123";
 		
-		Class.forName("com.mysql.jdbc.Driver");								// Step-2(b), 2(a) was done via GUI
+		Class.forName("com.mysql.cj.jdbc.Driver");									// Step-2(b), 2(a) was done via GUI
 		
-		Connection conn = DriverManager.getConnection(url, uname, pswrd);	// Step-3
+		Connection conn = DriverManager.getConnection(url, uname, pswrd);			// Step-3
+		// System.out.print(conn);	
 		
-		Statement stmt = conn.createStatement();							// Step-4
+		Statement stmt = conn.createStatement();									// Step-4
 		
-		ResultSet rs = stmt.executeQuery(" ");								// Step-5
+		// ResultSet rs = stmt.executeQuery("INSERT INTO department VALUES ('Jaydip')");
 		
-		while(rs.next()) {													// Step-6
-			System.out.println(rs.getString(1) + " " + rs.getString(2));
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM employee");	// Step-5
+		ResultSet rs = ps.executeQuery();
+		
+		ps = conn.prepareStatement("INSERT INTO employee VALUES (001910501016, 'Jaydip', 21, 'Mar21, 2022')");
+		ps.executeUpdate();
+		ps = conn.prepareStatement("INSERT INTO employee VALUES (001910501005, 'Atanu', 20, 'Jan01, 2022')");
+		ps.executeUpdate();
+		
+		while(rs.next()) {															// Step-6
+			System.out.println(rs.getString("department"));
 		}
 		
-		stmt.close();														// Step-7
+		stmt.close();																// Step-7
 		conn.close();
 	}
 }
